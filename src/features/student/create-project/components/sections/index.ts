d19a3a75b67c49e8c5cd509211314906a55168a9
Serviços Senai FSA <servicos.senaifsa@fieb.org.br>
2026-02-26 04:43:14 +0000

    fix: remover imports inativos que quebravam o build

--- src/features/student/create-project/components/sections/index.ts
+++ src/features/student/create-project/components/sections/index.ts
@@ -4,9 +4,3 @@ export { default as ProjectDetailsSection } from './ProjectDetailsSection'
 export { default as TeamSection } from './TeamSection'
 export { default as AttachmentsSection } from './AttachmentsSection'
 export { default as CodeSection } from './CodeSection'
-
-// Timeline/Phase sections
-export { default as IdeacaoSection } from './IdeacaoSection'
-export { default as ModelagemSection } from './ModelagemSection'
-export { default as PrototipagemSection } from './PrototipagemSection'
-export { default as ImplementacaoSection } from './ImplementacaoSection'
